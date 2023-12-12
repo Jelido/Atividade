d61c8e3ce3a8b5a256bc62cf8f31db8e1f83d73c
PAULO HENRIQUE PACHURA PENTEADO <paulo_penteado@estudante.sesisenai.org.br>
2023-12-12 19:31:10 -0300

    Base finalizada

--- Base/src/componentes/Main/index.jsx
+++ Base/src/componentes/Main/index.jsx
@@ -1,33 +1,14 @@
-import { Paragrafo, Cima, Input1, Input, Botoes, DivBotoes, Meio} from './style'
+import { Paragrafo, Cima} from './style'
 
 export function Main(){
     return(
         <>
         <Cima>
         
-        
-        <Paragrafo>Cadastro</Paragrafo>
+        <Paragrafo>Olá,Main</Paragrafo>
        
         </Cima>
 
-        <Meio>
-
-        <Input1 placeholder='Nome'></Input1>
-
-        <Input placeholder='Data de Nascimento'></Input>
-
-        <Input placeholder='E-mail'></Input>
-
-        <Input placeholder='CPF'></Input>
-
-        </Meio>
-        
-        <DivBotoes>
-
-        <Botoes>Cancelar</Botoes> <Botoes>Cadastrar</Botoes>
-
-        </DivBotoes>
-        
         </>
     )
 }
